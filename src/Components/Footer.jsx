a3efd66d633a66b9b161cd0d82dc5e7e0d8724f5
azmain-siam <siamhossain2700@gmail.com>
2024-04-30 23:14:00 +0600

    Final Commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/Logo.png";
 
 const Footer = () => {
   return (
-    <footer className="py-10 px-4 md:px-0 pb-0 text-base-content bg-[#ebebeb] dark:bg-base-100">
+    <footer className="py-10 px-4 md:px-0 pb-0 text-base-content bg-[#ebebeb]">
       <div className="max-w-7xl w-[95%] md:w-[93%] mx-auto">
         <div className="footer">
           <aside className="flex md:w-auto items-center gap-3">
@@ -14,13 +14,13 @@ const Footer = () => {
           </aside>
 
           <nav>
-            <h6 className="footer-title text-lg text-black dark:text-white">Services</h6>
+            <h6 className="footer-title text-lg text-black ">Services</h6>
             <a className="link link-hover">Customer Support</a>
             <a className="link link-hover">Shipping and Returns</a>
             <a className="link link-hover">Gift Cards</a>
           </nav>
           <nav>
-            <h6 className="footer-title text-lg text-black dark:text-white">Company</h6>
+            <h6 className="footer-title text-lg text-black">Company</h6>
             <a className="link link-hover">About us</a>
             <Link to={"/contact"} className="link link-hover">
               Contact
@@ -28,7 +28,7 @@ const Footer = () => {
             <a className="link link-hover">Jobs</a>
           </nav>
           <nav>
-            <h6 className="footer-title text-lg text-black dark:text-white">Legal</h6>
+            <h6 className="footer-title text-lg text-black ">Legal</h6>
             <a className="link link-hover">Terms of use</a>
             <a className="link link-hover">Privacy policy</a>
             <a className="link link-hover">Cookie policy</a>
@@ -36,7 +36,7 @@ const Footer = () => {
         </div>
 
         <hr className="mt-10 mb-5 border-[#cacaca]" />
-        <aside className="footer-center pb-5 dark:text-white text-sm font-medium text-gray-600">
+        <aside className="footer-center pb-5  text-sm font-medium text-gray-600">
           <p>Copyright © 2024 | All right reserved by ArtFusion Properties.</p>
         </aside>
       </div>
